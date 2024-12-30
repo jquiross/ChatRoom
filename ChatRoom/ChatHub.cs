3a3f50e6cbe1735fd9548acddd85d45cd5d40359
jquiross <jeremyquiros03@gmail.com>
2024-12-30 13:39:02 -0600

    feat.all
    whole functionality

---
+++ ChatRoom/ChatHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChatRoom
+{
+    public class ChatHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+            // Call the broadcastMessage method to update clients.
+            Clients.All.Sendchat(name, message);
+        }
+    }
+}
---
+++ ChatRoom/Startup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Threading.Tasks;
+
+[assembly: OwinStartup(typeof(ChatRoom.Startup))]
+
+namespace ChatRoom
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
